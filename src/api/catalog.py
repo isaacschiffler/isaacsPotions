11850d1d20885e163dd19dcd56a2dde520f9b51a
Isaac Schiffler <ischiffl@calpoly.edu>
2024-04-08 16:25:40 -0700

    changed catalog to return json object only when quantity > 0

--- src/api/catalog.py
+++ src/api/catalog.py
@@ -10,22 +10,23 @@ def get_catalog():
     """
     Each unique item combination must have only a single price.
     """
+    my_catalog = []
 
     '''use start database connection green_potion_inventory = get current inventory of green potions'''
     with db.engine.begin() as connection:
         result = connection.execute(sqlalchemy.text("SELECT * FROM global_inventory"))
         row = result.fetchone()
         num_green_potions = row[2]
+        print(num_green_potions)
         if num_green_potions > 0:
             num_green_potions = 1
-
-
-    return [
-            {
+            my_catalog.append({
                 "sku": "GREEN_POTION_0",
                 "name": "green potion",
                 "quantity": num_green_potions,
                 "price": 48,
                 "potion_type": [0, 100, 0, 0],
-            }
-        ]
+            })
+
+
+    return my_catalog
