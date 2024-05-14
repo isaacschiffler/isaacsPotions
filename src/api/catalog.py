6de91a7acdaa18f286fbf9956b3c3011d792e4ed
Isaac Schiffler <ischiffl@calpoly.edu>
2024-05-14 09:57:13 -0700

    gotta sell orange

--- src/api/catalog.py
+++ src/api/catalog.py
@@ -19,7 +19,8 @@ def get_catalog():
                                                     FROM potion_types 
                                                     JOIN potions ON potion_types.type = potions.type
                                                     WHERE potions.quantity > 0 
-                                                    ORDER BY quantity ASC;""")).fetchall()
+                                                    ORDER BY potions.quantity ASC
+                                                    LIMIT 10 OFFSET 2;""")).fetchall()
         
         for row in result:
             if row.type == [0, 1, 0, 0]:
