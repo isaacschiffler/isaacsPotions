5018aee335f43053269855566b1bb0a1550cc55a
Isaac Schiffler <ischiffl@calpoly.edu>
2024-05-16 09:53:12 -0700

    no red today

--- src/api/catalog.py
+++ src/api/catalog.py
@@ -23,7 +23,7 @@ def get_catalog():
                                                     LIMIT 10;""")).fetchall()
         
         for row in result:
-            if row.type == [50, 50, 0, 0]:
+            if row.type == [100, 0, 0, 0]:
                 # no ____ today!
                 continue
             print("Adding to catalog: " + str(row))
